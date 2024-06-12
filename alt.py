b5338642aacf9ad6507728cb4182cfd5533a0950
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-12 19:44:43 -0300

    Update alt.py

--- alt.py
+++ alt.py
@@ -1,1 +1,15 @@
 
+import tkinter as tk
+from tkinter import messagebox
+
+def show_alert():
+    root = tk.Tk()
+    root.withdraw()  # Oculta a janela principal
+    messagebox.showinfo('Alerta', 'Esta é uma mensagem de alerta!')
+    root.destroy()
+
+show_alert()
+
+
+
+
