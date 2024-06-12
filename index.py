3f8036b9105f1b6e2fc6019688684cdd1b912a67
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-12 19:38:03 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,22 @@
 
+import schedule
+import time
+from plyer import notification
+
+def alert(title, message):
+    notification.notlify(
+        title=title,
+        message=message,
+        app_name='Alerta do Windows',
+        timeout=1
+    )
+
+    def msg():
+        alert('Alerta do windows', 'hora de caralhar!!')
+
+    if __name__=="__main__":
+        schedule.every().day.at("19:36").do(msg)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)    
